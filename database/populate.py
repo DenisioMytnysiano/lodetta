327679a8983172b136016cd8f3356f0a9bdce828
DenisioMytnysiano <den.mytnyk@ua.fm>
2021-11-07 21:44:15 +0200

    Added database restore script

--- database/populate.py
+++ database/populate.py
@@ -1,41 +1,54 @@
 import os
+import logging
 import asyncio
 from typing import List, Dict, NoReturn
-import motor.motor_asyncio
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorDatabase,
+    AsyncIOMotorCollection,
+)
+
+logging.root.setLevel(logging.DEBUG)
+logging.basicConfig()
 
 FILENAME = os.environ.get("FILENAME")
 MONGO_USER = os.environ.get("MONGO_USER")
 MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
 MONGO_PORT = os.environ.get("MONGO_PORT")
 HOSTNAME = os.environ.get("HOSTNAME")
-print(MONGO_PORT)
 MONGODB_CONNECTION_STRING = (
     f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}" + f"@{HOSTNAME}:{MONGO_PORT}"
 )
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_CONNECTION_STRING)
-database = client["lodetta"]
-collection = database["logos"]
+client: AsyncIOMotorClient = AsyncIOMotorClient(MONGODB_CONNECTION_STRING)
+database: AsyncIOMotorDatabase = client["lodetta"]
+collection: AsyncIOMotorCollection = database["logos"]
 
 
 def extract_logos(filename: str) -> List[Dict]:
     logos = []
     with open(filename, "r") as source:
         for line in source:
-            logo = {"name": line.strip()}
+            id, name = line.strip().split(",")
+            logo = {"id": int(id), "name": name, "status": "Supported"}
             logos.append(logo)
     return logos
 
 
-async def insert_logos(logos: List[Dict]) -> NoReturn:
-    await collection.insert_many(logos)
+async def setup_database(data: List[Dict]) -> NoReturn:
+    await collection.insert_many(data)
+    logging.info(f"Inserted total {len(data)} in {collection} collection")
+    collection.create_index("id", unique=True)
+    logging.info("Created unique index on 'id' field")
+    collection.create_index("name", unique=True)
+    logging.info("Created unique index on 'name' field")
 
 
 def main():
     loop = asyncio.get_event_loop()
     logos = extract_logos(FILENAME)
-    loop.run_until_complete(insert_logos(logos))
+    loop.run_until_complete(setup_database(logos))
 
 
 if __name__ == "__main__":
